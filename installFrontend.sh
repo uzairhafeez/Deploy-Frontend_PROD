43f1fb53bb8b8cb73c1e423abcb1256f88f752ec
uzairhafeez <muh3@njit.edu>
2017-05-09 21:18:00 -0400

    Fixed .ini file before installing the package

--- installFrontend.sh
+++ installFrontend.sh
@@ -1,14 +1,21 @@
 echo "UNZIP BUNDLE_FE"
 
+# packageNum=`php currentPackage.php | xargs`
 package="$1"
 
 mkdir $HOME/Packages/Unzip/
+# cd $HOME/Packages/Unzip/
 
 tar -xzf $HOME/Packages/$package -C /home/uzair/Packages/Unzip
 
+echo "Adjusting RabbitMQ .ini..."
+
+sed -i '2s/.*/BROKER_HOST = 192.168.2.21/g' $HOME/Packages/Unzip/FE/testRabbitMQ.ini
+
 sudo cp -a $HOME/Packages/Unzip/FE/. /var/www/html/
-echo "Successfully installed"
 
 echo -e "\n"
-echo "Success"
 
+rm -r $HOME/Packages/frontend/Unzip
+
+echo "Frontend Package is sucessfully installed!"
